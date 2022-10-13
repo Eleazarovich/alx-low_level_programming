91958e1419c993583aa666a60f8c47bef44cd7b3
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-13 18:55:38 +0000

    Added a 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the sizes of various c data types 
+ *Return: 0
+ */
+
+int main(void)
+{
+	char char_size;
+	int int_size;
+	long int long_int_size;
+	long long int long_long_int_size;
+	float float_size;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char_size));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int_size));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_size));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long_long_int_size));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float_size));
+
+	return (0);
+}
