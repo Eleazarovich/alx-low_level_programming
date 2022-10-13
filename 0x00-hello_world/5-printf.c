6988a7b0d0f07f1310e9a97eb9f98c9a260d801e
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-13 13:08:46 +0000

    Modified a 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - Prints a string to standard output
-*/
+ *main - Prints a string to standard output
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
