8d1d98c965112c3f17c968286af0a4fbffec4370
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-13 12:45:08 +0000

    Modified 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+*main uses puts to print a string to stadard output
+*main takes no argument 
+*main returns 0
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
