9402a49367d5b1455fcfc14643aac8d87ec2d428
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-13 19:35:16 +0000

    Added a 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string to std error
+ * Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\"- Dora korpar, 2015-10-19\n");
+
+	return (0);
+}
