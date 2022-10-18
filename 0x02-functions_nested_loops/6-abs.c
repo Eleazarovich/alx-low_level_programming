d6735c17d8359aea611f97a330a3887130115f9b
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-18 21:04:28 +0000

    Modified a file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 
 /**
  * _abs - Computes the absolute value
- * @int: Takes in an integer
- * Return: -n
+ * @n: Takes in an integer
+ * Return: abs of a number
  */
 
 int _abs(int n)
 {
-	if (n >= 0) 
+	if (n >= 0)
 	{
 		return (n);
 	}
