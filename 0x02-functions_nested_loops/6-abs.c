69a0195b994da03baf0f4f30cf521eff6158cadc
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-18 20:38:28 +0000

    Added a file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value
+ * @int: Takes in an integer
+ * Return: -n
+ */
+
+int _abs(int);
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
