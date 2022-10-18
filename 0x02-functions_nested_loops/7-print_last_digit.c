fda35effeddac6203e340982ab85ddbd793abe38
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-18 21:18:35 +0000

    Added a file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a num
+ * @n: Takes in an integer
+ * Return: n
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = (n % 10);
+
+	_putchar('l');
+
+	return (l);
+}
+
+
