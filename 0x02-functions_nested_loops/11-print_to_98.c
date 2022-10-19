b652a19899437113504ce9ff68d037652772081d
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-19 16:58:44 +0000

    Added a file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,28 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
 		}
 	}
 	else
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
 		}
 	}
 }
