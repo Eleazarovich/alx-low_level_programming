4434ca45322bbcc2a184a640cff9fd01d6c31544
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-20 06:01:41 +0000

    Modified Test 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	while (i < 10) /* don't delete it! */
 	{
-		putchar(i); /* don't delete it! */
+		putchar(i);
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
