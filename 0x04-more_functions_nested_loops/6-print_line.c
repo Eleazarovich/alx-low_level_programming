a450006112b1ffa577b29d5134d12a5d3dd9b246
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-20 16:21:39 +0000

    Modified 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
 	else
 	{
 		int i;
 
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
