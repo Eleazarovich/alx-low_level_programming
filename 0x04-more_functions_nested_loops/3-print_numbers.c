056a63fd8d78b82559876a82e9b71f8811786a23
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-20 15:54:54 +0000

    New program

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints nums (0-9)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= 'n'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
