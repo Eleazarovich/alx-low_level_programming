892e65ab96ba0b6e892e30890827cad07958adc7
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-20 22:09:33 +0000

    Modified 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - FizzBuzz ptogram
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	int i;
@@ -12,7 +17,7 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			if ( i != 100)
+			if (i != 100)
 			{
 				printf("Buzz ");
 			}
