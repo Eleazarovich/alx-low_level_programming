7ad1122fa65b6bdf2991c53715c2b302e637ef6d
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-20 16:10:50 +0000

    Modified 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
 	{
 		int i, j;
 
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
