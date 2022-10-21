e51818fe653041f672a7ef0ecb7508e5a5cdaa19
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-21 22:18:19 +0000

    New Program

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: Takes in an integer
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-')
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + 48);
+
+	_putchar('\n');
+
+	return (0);
+}
