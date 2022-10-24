feef7e750dbb6b10187e7155de86652e200881fd
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-24 12:58:09 +0000

    New Program 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: fisrt pointer
+ * @b: second pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	c = *a, d = *b;
+	*a = d, *b = c;
+}
