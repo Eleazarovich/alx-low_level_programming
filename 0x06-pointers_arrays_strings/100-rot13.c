d22ea22bda3f5dbd167277419554f4882e9d8b30
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-27 23:08:19 +0000

    New Program 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: s
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = o; s[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[y])
+			{
+				s[i] = rot[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
