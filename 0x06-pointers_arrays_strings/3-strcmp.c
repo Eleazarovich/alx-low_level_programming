28c326d40ad85ec30fc84d9bc16d3917498fd147
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-10-27 12:18:15 +0000

    New Program

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, r;
+
+	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+	}
+	return (r);
+}
