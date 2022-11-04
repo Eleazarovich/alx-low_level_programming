18d77aa81b867930edbfb07a026d6ef6d9e3f8be
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-04 19:23:49 +0000

    modified 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: 0 for success
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
