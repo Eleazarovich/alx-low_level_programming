b20f301760df1e2550717a7bd71fe6b5c5d0987a
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-13 07:44:16 +0000

    bug fixes for program 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,24 +29,26 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i;
+	unsigned int i, strlen1;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+	
+	strlen1 = _strlen(s1);
 
-	s = malloc(sizeof(char) * _strlen(s1) + n + 1);
+	s = malloc(sizeof(char) * strlen1 + n + 1);
 
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < _strlen(s1); i++)
+	for (i = 0; i < strlen1; i++)
 		s[i] = s1[i];
 
 	for (i = 0; i < n; i++)
-		s[_strlen(s1) + i] = s2[i];
+		s[strlen1 + i] = s2[i];
 
 	return (s);
 }
