92ddd03ece0419a85634dc3a8bcd6e6cac054d58
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-15 12:24:52 +0000

    header file 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog properties
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
