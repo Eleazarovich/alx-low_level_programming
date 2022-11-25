f69bca98a4290afdfdf628ff281e3a93ef4c3b14
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-25 19:59:39 +0000

    modified prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * struct lists_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string
  * @len: length of the string
  * @new: points to the next node
@@ -16,7 +16,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next;
+	struct list_s *new;
 } list_t;
 
 size_t print_list(const list_t *h);
