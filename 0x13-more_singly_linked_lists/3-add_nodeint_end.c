1c46499f48c486998d7a08fd31a68392ee3d0537
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-29 10:05:52 +0000

    modified program 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	ptr = *head;
 
-	while (ptr->next != NULL)
+	while (ptr && ptr->next != NULL)
 		ptr = ptr->next;
 	ptr->next = new_node;
 
