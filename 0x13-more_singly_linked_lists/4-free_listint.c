c6cf74ff3a677358b6f472b80a12c9a43305b4e6
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-29 10:34:12 +0000

    modified program 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	ptr = head;
-
-	while (ptr != NULL)
+	while (head != NULL)
 	{
+		ptr = head;
+		head = head->next;
 		free(ptr);
-		ptr = ptr->next;
 	}
 }
