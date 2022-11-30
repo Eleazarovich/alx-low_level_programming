45f41acb8e49e93772cb949f98acea06ce6bc943
Eleazar Nhamuave <eleazarovich176@gmail.com>
2022-11-30 07:53:13 +0000

    modified program 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,10 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		ptr = (*head)->next;
 		free(*head);
 		*head = ptr;
 	}
-	head = NULL;
 }
